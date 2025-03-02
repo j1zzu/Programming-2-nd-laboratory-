2511acc5d56db123c968d7fb600d0483195d8c63
j1zzu <101795751+j1zzu@users.noreply.github.com>
2025-03-02 17:06:39 +0300

    Add files via upload

---
+++ codes of 9 laboratory/task-1/task-1/task-1.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+int main() {
+    setlocale(LC_ALL, "RUS");
+    string date;
+    string date_piece;
+    string months[] = { "Январь", "Февраль", "Март", "Апрель", "Май", "Июнь", "Июль", "Август", "Сентябрь", "Октябрь", "Ноябрь", "Декабрь" };
+    cout << "Введите дату (формата MM/DD/YY): "; cin >> date;
+    for (int i = 0; i < date.length(); i++) {
+        if (i == 2) {
+            if (stoi(date_piece) < 13) {
+                cout << *(months + stoi(date_piece) - 1) << ' ';
+            }
+            else {
+                cout << "\nНеправильно введен месяц.";
+                return 1;
+            }
+            date_piece = "";
+            continue;
+        }
+        else if (i == 5) {
+            if (stoi(date_piece) < 32) {
+                cout << date_piece << ", ";
+            }
+            else {
+                cout << "\nНеправильно введен день.";
+                return 2;
+            }
+            date_piece = "";
+            continue;
+        }
+        else if (i == 7) {
+            date_piece += date[i];
+            cout << stoi(date_piece) + 2000;
+            break;
+        }
+        date_piece += date[i];
+    }
+    return 0;
+}
---
+++ codes of 9 laboratory/task-2/task-2/task-2.cpp
@@ -0,0 +1,46 @@
+﻿#include <iostream>
+using namespace std;
+int main() {
+    system("chcp 1251");
+    string faculty;
+    int group_count;
+    cout << "Введите название факультета: ";
+    cin >> faculty;
+    cout << "Введите число групп: ";
+    cin >> group_count;
+    // Динамическое выделение памяти
+    string* names = new string[group_count];
+    double** grades = new double* [group_count];
+
+    for (int i = 0; i < group_count; i++) {
+        grades[i] = new double[5];
+    }
+    for (int i = 0; i < group_count; i++) {
+        cout << "Введите название для " << i + 1 << "-й группы: ";
+        cin >> names[i];
+    }
+    cout << "Заполните данные о группах (Через пробел, процент пятерок, четверок, троек, двоек и успеваемости группы): \n";
+    for (int i = 0; i < group_count; i++) {
+        cout << names[i] << ' ';
+        cin >> grades[i][0];
+        cin >> grades[i][1];
+        cin >> grades[i][2];
+        cin >> grades[i][3];
+        cin >> grades[i][4];
+    }
+
+    cout << "\nУспеваемость по группам факультета " << faculty << '\n';
+    cout << "Группа | % пятерок | % четверок | % троек | % двоек | % успеваемости группы\n";
+    for (int i = 0; i < group_count; i++)
+        cout << names[i] << ' ' << grades[i][0] << ' '\
+        << grades[i][1] << ' ' << grades[i][2] << ' ' \
+        << grades[i][3] << ' ' << grades[i][4] << '\n';
+    cout << "Средний % по факультету ";
+    for (int i = 0; i < 5; i++) {
+        double value = 0;
+        for (int j = 0; j < group_count; j++)
+            value += grades[j][i];
+        cout << value / group_count << ' ';
+    }
+    return 0;
+}
