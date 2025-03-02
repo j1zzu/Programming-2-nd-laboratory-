3746f8039958534cb139c28df24f2695c01f070a
j1zzu <101795751+j1zzu@users.noreply.github.com>
2025-03-02 16:36:55 +0300

    Add files via upload

---
+++ codes of 2 laboratory/1task-square area/1task-square area/1task-square area.cpp
@@ -0,0 +1,13 @@
+﻿#include<iostream>
+#include<cmath>
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+    //a - сторона квадрата, S - площадь квадрата
+    int a, S;
+    std::cout << "Введите сторону квадрата: \na = ";
+    std::cin >> a;
+
+    S = pow(a, 2);
+    std::cout << "Площадь квадрата со сторонами " << a << " равна " << S << std::endl;
+}
