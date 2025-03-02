dfe96dc76dee49ebcdab57db563a3cbab2511ae8
j1zzu <101795751+j1zzu@users.noreply.github.com>
2025-03-02 16:59:02 +0300

    Add files via upload

---
+++ codes of 6 laboratory/task-1/task-1/task-1.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "Rus");
+    ifstream file("FileName.txt"); // Открываем файл для чтения
+    string text; // Переменная для хранения текста из файла
+    string word; // Переменная для хранения текущего слова
+
+    // Читаем файл слово за словом
+    while (file >> word) {
+        // Проверка, является ли первая буква гласной
+        if (word[0] == 'a' || word[0] == 'e' || word[0] == 'i' || word[0] == 'o' || word[0] == 'u') {
+            word[0] = toupper(word[0]); // Преобразуем первую букву в заглавную
+        }
+        text += word + " "; // Добавляем слово к тексту с пробелом
+    }
+
+    file.close(); // Закрываем файл
+
+    // Выводим результат на экран
+    cout << "Изменение регистра:\n" << text << endl;
+}
---
+++ codes of 6 laboratory/task-2/task-2/task-2.cpp
@@ -0,0 +1,46 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "Rus");
+    ifstream file("input.txt"); // Открываем файл для чтения
+    string maxSentence; // Переменная для хранения предложения с максимальным количеством знаков пунктуации
+    int maxpunctuatCount = 0; // Максимальное количество знаков пунктуации
+    string currentSentence; // Текущее предложение
+    int punctuatCount = 0; // Количество знаков пунктуации в текущем предложении
+    char ch; // Хранение одного символа
+    string text;
+
+    // Чтение текста посимвольно
+    while (file.get(ch)) {
+        currentSentence += ch; // Добавляем символ к текущему предложению
+
+        // Проверка, является ли символ знаком пунктуации
+        if (ch == '.' || ch == ',' || ch == '!' || ch == '?' || ch == ':' || ch == ';' || ch == '-' || ch == '"') {
+            punctuatCount += 1;
+        }
+
+        // Проверка знака препинания в конце предложения
+        if (ch == '.' || ch == '!' || ch == '?') {
+            // Сравниваем количество знаков пунктуации
+            if (punctuatCount > maxpunctuatCount) {
+                maxpunctuatCount = punctuatCount;
+                maxSentence = currentSentence;
+            }
+
+            // Обнуление предложения и счётчика
+            text += currentSentence;
+            currentSentence = "";
+            punctuatCount = 0;
+        }
+    }
+
+    file.close();
+    cout << "Полный текст: " << text << endl;
+
+    cout << "\nПредложение с максимальным количеством знаков пунктуации:\n" << maxSentence << endl;
+
+}
