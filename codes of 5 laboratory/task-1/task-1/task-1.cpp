564632ed6ebbcf4c4346600c2a600fb229a4013c
j1zzu <101795751+j1zzu@users.noreply.github.com>
2025-03-02 16:53:10 +0300

    Add files via upload

---
+++ codes of 5 laboratory/task-1/task-1/task-1.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+	const int row = 4;
+	const int col = 3;
+	int mass[row][col] = { {1,2,-3}, {3,-5,7}, {-2,-1,1}, {6,5,-1} };
+	int positive[col] = { 0 };
+
+	for (int j = 0; j < col; j++) {
+		for (int i = 0; i < row; i++) {
+			if (mass[i][j] > 0) {
+				positive[j] += mass[i][j];
+			}
+		}
+	}
+
+	for (int i = 0; i < col; i++) {
+		cout << positive[i] << " ";
+	}
+}
---
+++ codes of 5 laboratory/task-2/task-2/task-2.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+	const int row = 4;
+	const int col = 3;
+	int mass[row][col] = { {1,2,3}, {3,5,7}, {2,1,1}, {6,5,1} };
+	int max_value = -10000;
+	int min_value = 10000;
+
+	for (int i = 0; i < row; i++) {
+		for (int j = 0; j < col; j++) {
+			if (mass[i][j] > max_value) {
+				max_value = mass[i][j];
+			}
+			if (mass[i][j] < min_value) {
+				min_value = mass[i][j];
+			}
+		}
+	}
+	cout << (max_value + min_value) / 2;
+}
